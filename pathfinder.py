ad603763d224a8963c42ebc898127d6ea52869f3
Athul P <athul929@gmail.com>
2020-12-05 00:07:08 +0530

    path tracking implemented

--- pathfinder.py
+++ pathfinder.py
@@ -1,8 +1,8 @@
-#%%
 import cv2 as cv
 from matplotlib import pyplot as plt
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
+
 
 class PathFinder:
     def __init__(self, simWidth, simHeight, simX, simY):
@@ -13,21 +13,21 @@ class PathFinder:
 
     def find(self, img):
         preproc = self.preprocess(img)
-        #7.34 and 9.04 are actual dimension in the sim env
+        # 7.34 and 9.04 are actual dimension in the sim env
         self.createMapping(7.34/self.width, 9.04/self.height)
         grid = Grid(matrix=preproc)
         start = grid.node(287, 44)
         end = grid.node(52, 420)
         finder = AStarFinder()
         path, _ = finder.find_path(start, end, grid)
+        path = path[0::4]
+        self.path = [[p[0], p[1]] for p in path]
+        path = self.smooth(10, 0.4, 50)
         actualPath = []
         for point in (path):
-            x = self.mapping[(point[0], point[1])][0]
-            y = self.mapping[(point[0], point[1])][1]
-            actualPath.append((x,y,0.33))
-        # convert tuple to list
-        path = path[0::4]
-        self.path = [[p[0],p[1]] for p in path]
+            x = self.mapping[(int(point[0]), int(point[1]))][0]
+            y = self.mapping[(int(point[0]), int(point[1]))][1]
+            actualPath.append((x, y))
         return actualPath
 
     def imshow(self, image):
@@ -49,23 +49,22 @@ class PathFinder:
             for i in range(1, len(self.path)-1):
                 for j in range(2):
                     aux = newpath[i][j]
-                    newpath[i][j] += amount*(self.path[i][j] - newpath[i][j]) + smoothness*(newpath[i-1][j] + newpath[i+1][j] - (2.0*newpath[i][j]))
+                    newpath[i][j] += amount*(self.path[i][j] - newpath[i][j]) + smoothness*(
+                        newpath[i-1][j] + newpath[i+1][j] - (2.0*newpath[i][j]))
                     change += abs(aux - newpath[i][j])
-            print(change)
         self.path = newpath
         return newpath
 
-
     def preprocess(self, img):
         # flip the image to match the simulator
-        img = cv.flip(img,1)
+        img = cv.flip(img, 1)
         # crop to the boundary
-        img = img[ 17:495,50:475]
+        img = img[17:495, 50:475]
         # cv.imwrite("initial.png", img)
         self.img = img
         self.height = (len(img))
         self.width = (len(img[0]))
-        edges = cv.Canny(img, 10 ,50)
+        edges = cv.Canny(img, 10, 50)
         edges = (255-edges)
         # cv.imwrite("edges.png", edges)
         self.img = img
@@ -73,14 +72,13 @@ class PathFinder:
         # cv.imwrite("increased.png", increased)
         return increased
 
-
     def createMapping(self, xStep, yStep):
         self.mapping = dict()
         for i in range(self.width):
             for j in range(self.height):
-                # 3.67 and -4.72 
-                self.mapping[(i,j)] = (3.67 - i*xStep, -4.72 + j*yStep)
-            
+                # 3.67 and -4.72
+                self.mapping[(i, j)] = (3.67 - i*xStep, -4.72 + j*yStep)
+
     def incBoundWidth(self, edges, incWidth):
         # Increase horizontally
         for i in range(self.height):
@@ -89,7 +87,7 @@ class PathFinder:
                 if edges[i][j] == 0:
                     temp = j
                     count = 0
-                    while temp >=0 and count < incWidth:
+                    while temp >= 0 and count < incWidth:
                         edges[i][temp] = 0
                         temp -= 1
                         count += 1
@@ -99,14 +97,14 @@ class PathFinder:
                         j = j + 1
                         count = count + 1
                 j = j + 1
-        #increace veritaclly
+        # increace veritaclly
         for i in range(self.width):
             j = 0
             while j < self.height:
                 if edges[j][i] == 0:
                     temp = j
                     count = 0
-                    while temp >=0 and count < incWidth:
+                    while temp >= 0 and count < incWidth:
                         edges[temp][i] = 0
                         temp -= 1
                         count += 1
@@ -117,15 +115,3 @@ class PathFinder:
                         count = count + 1
                 j = j + 1
         return edges
-
-#%%
-img = cv.imread("./test.png", cv.IMREAD_GRAYSCALE)
-finder = PathFinder(0,0,0,0)
-finder.find(img)
-#%%
-finder.smooth(10, 0.4, 50)
-
-# %%
-finder.visualizePath()
-
-# %%
--- pathtracker.py
+++ pathtracker.py
@@ -1,11 +1,45 @@
 import math
+from robot import Robot
+import numpy as np
+from numpy.core.numeric import NaN
 
 
 class PathTracker:
-    def __init__(self, path):
+    def __init__(self, path, lookdist, width, robot):
+        self.lookdist = lookdist
         self.path = path
+        self.prevLookahead = None
+        self.prevLookindex = -0.5
+        self.angle = robot.getAngle()
+        self.pos = robot.getPos()[0:2]
+        self.pos[0] += 1
+        # self.pos = [0,0]
+        self.width = width
+        self.dt = 0.005
+        self.robot = robot
 
-    def curvature(self, point, prev, after):
+    def track(self):
+        while True:
+            lookpoint = self.lookhead(self.pos)
+            curv = self.curvature(lookpoint)
+            wheels = self.turn(curv, 3, self.width)
+            print(wheels)
+            self.robot.setLeftVelocity(wheels[0])
+            self.robot.setRightVelocity(-wheels[1])
+            # while(lookpoint[0] > pos[0] and lookpoint[1] > pos[1]):
+            #     pos = self.robot.getPos()
+            # self.robot.setLeftVelocity(0)
+            # self.robot.setRightVelocity(0)
+            self.pos = self.robot.getPos()[0:2]
+            self.angle = self.robot.getAngle()
+            [a_i - b_i for a_i, b_i in zip(self.pos,list(self.path[-1]))]
+            # if (np.linalg.norm([a_i - b_i for a_i, b_i in zip(self.pos,list(self.path[-1]))]) < 2):
+            #     break
+    
+    def turn(self,curv, vel, trackwidth):
+        return  [vel*(2+curv*trackwidth)/2, vel*(2-curv*trackwidth)/2]
+
+    def curve(self, point, prev, after):
         x1, y1 = point[0], point[1]
         x1 += 0.00001  # divide by zero error
         x2, y2 = after[0], after[1]
@@ -18,5 +52,44 @@ class PathTracker:
         r = math.sqrt(pow(x1-a, 2) + pow(y1-b, 2))
         return 1/r
 
+    def curvature(self, lookapoint):
+        side = np.sign(math.sin(self.angle)*(lookapoint[0]-self.pos[0]) - math.cos(
+            self.angle)*(lookapoint[1]-self.pos[1]))
+        a = -math.tan(self.angle)
+        c = math.tan(self.angle)*self.pos[0] - self.pos[1]
+        x = abs(a*lookapoint[0] + lookapoint[1] + c) / math.sqrt(a**2 + 1)
+        return side * (2*x/(self.lookdist**2))
+
     def distance(self, point1, point2):
-        return math.sqrt(pow(point1[0]-point2[1], 2) + pow(point1[2] - point2[1], 2))
+        return np.linalg.norm(point1 - point2)
+
+    def lookhead(self, robot_pos):
+        for i in range(math.ceil(self.prevLookindex), len(self.path)-1):
+            line_start = self.path[i]
+            line_end = self.path[i+1]
+            d = np.subtract(line_end, line_start)
+            f = np.subtract(line_start, robot_pos)
+
+            a = np.dot(d, d)
+            b = np.dot(2*f, d)
+            c = np.dot(f, f)-(self.lookdist*self.lookdist)
+
+            discriminant = b*b-(4*a*c)
+
+            if discriminant >= 0:
+                discriminant = math.sqrt(discriminant)
+                t1 = (-b - discriminant)/(2 * a)
+                t2 = (-b + discriminant)/(2 * a)
+                if 0 <= t1 <= 1:
+                    self.prevLookindex = i + t1
+                    self.prevLookahead = line_start + t1*d
+                    return self.prevLookahead
+                if 0 <= t2 <= 1:
+                    self.prevLookindex = i + t2
+                    self.prevLookahead = line_start + t2*d
+                    return self.prevLookahead
+        return self.prevLookahead
+
+# tracker = PathTracker([[0,0], [1,1]], 1, 0.001, None)
+# print(tracker.lookhead([0,0]))
+# print(tracker.curvature([0.707, 0.707]))
